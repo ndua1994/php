1bfe40d089f698dacaee433b695d2fef61b96cf9
ndua1994 <duanishant71@gmail.com>
2022-10-07 20:11:18 +0530

    session 11 and 12

---
+++ session10.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Session 10</title>
+</head>
+<body>
+
+
+<?php
+
+/*$names=array('nishant','arjun','bablu');
+sort($names);
+
+foreach($names as $name)
+{
+	echo $name.'<br>';
+}
+*/
+
+/*$number=array(87,5,1,53);
+rsort($number);
+
+foreach($number as $num)
+{
+	echo $num.'<br>';
+}*/
+/*
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+krsort($age);
+
+foreach($age as $x => $x_value) {
+  echo "Key=" . $x . ", Value=" . $x_value;
+  echo "<br>";
+}
+*/
+
+
+?>
+
+
+
+</body>
+</html>
---
+++ session11.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>session 11</title>
+</head>
+<body>
+
+
+<?php
+/*echo '<pre>';
+print_r($_SERVER);*/
+
+
+
+/*$num1=10;
+$num2=20;
+
+
+function add()
+{
+	return $GLOBALS['num1']+$GLOBALS['num2'];
+
+}
+
+
+echo add();*/
+
+/*
+print_r($_FILES);*/
+
+?>
+
+</body>
+</html>
