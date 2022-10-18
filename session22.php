e6d9db0310004a915d5051de1b23231d2ded52cb
ndua1994 <duanishant71@gmail.com>
2022-10-18 19:44:44 +0530

    mysql connection added

---
+++ session22.php
@@ -0,0 +1,21 @@
+<?php
+
+$conn=mysqli_connect('localhost','root','','php_practice');
+if(!$conn)
+{
+	die('Databse Connection Failed'.mysqli_error());
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>mysql connection</title>
+</head>
+<body>
+
+</body>
+</html>
